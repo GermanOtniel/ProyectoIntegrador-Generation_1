791cd2acbc013e4060f170cd076feb9de3f775e2
MiguelSantiagoV <mayestix12@gmail.com>
2021-10-14 14:24:30 -0600

    Formulario de contacto.

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,3 +1,27 @@
+function register(){
+  //Declarando variables
+  var formulario_login = document.querySelector(".formulario__login");
+  var formulario_register = document.querySelector(".formulario__register");
+  var contenedor_login_register = document.querySelector(".contenedor__login-register");
+  var caja_trasera_login = document.querySelector(".caja__trasera-login");
+  //var caja_trasera_register = document.querySelector(".caja__trasera-register");
+  if (window.innerWidth > 850){
+      formulario_register.style.display = "block";
+      contenedor_login_register.style.left = "410px";
+      formulario_login.style.display = "none";
+      //caja_trasera_register.style.opacity = "0";
+      caja_trasera_login.style.opacity = "1";
+  }else{
+      formulario_register.style.display = "block";
+      contenedor_login_register.style.left = "0px";
+      formulario_login.style.display = "none";
+      //caja_trasera_register.style.display = "none";
+      caja_trasera_login.style.display = "block";
+      caja_trasera_login.style.opacity = "1";
+  }
+}
+
+register();
 
 function validation() {
   let name = document.getElementById("contact-us-name");
