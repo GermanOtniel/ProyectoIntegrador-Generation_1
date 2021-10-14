f6ce467a7f6c95de9b632ae8b97b8028148104c0
GermanOtniel <otnielgs94@gmail.com>
2021-10-13 19:50:15 -0500

    Done, login-register

--- js/login-register.js
+++ js/login-register.js
@@ -62,3 +62,189 @@ anchoPage();
             caja_trasera_login.style.opacity = "1";
         }
 }
+
+const buttonRegisterAction = document.getElementById('action-register');
+const fullNameInput = document.getElementById('fullname-reg');
+const emailInput = document.getElementById('email-reg');
+const passwordInput = document.getElementById('pass-reg');
+const passwordConfirmInput = document.getElementById('pass-confirm-reg');
+const selectLoginBtn = document.getElementById('btn__iniciar-sesion');
+const selectRegisterBtn = document.getElementById('btn__registrarse');
+const errFName = document.getElementById('error-fullname');
+const errEmail = document.getElementById('error-email');
+const errPass = document.getElementById('error-pass');
+
+const emailLogin = document.getElementById('login-email');
+const passwordLogin = document.getElementById('login-pass');
+const actionBtnLogin = document.getElementById('action-login');
+const textErrorEmailLogin = document.getElementById('error-email-login');
+const textErrorPassLogin = document.getElementById('error-pass-login');
+
+const resetFormValues = () => {
+    [fullNameInput, emailInput, passwordInput, passwordConfirmInput, emailLogin, passwordLogin].forEach((input) => {
+        input.value = '';
+        input.classList.remove('is-invalid');
+    });
+
+    [errFName, errEmail, errPass, textErrorEmailLogin, textErrorPassLogin].forEach((errorText) => {
+        errorText.innerText = '';
+    });
+};
+
+selectLoginBtn.addEventListener('click', function (e) {
+    resetFormValues();
+});
+
+selectRegisterBtn.addEventListener('click', function (e) {
+    resetFormValues();
+});
+
+const renderErrorMsg = () => {
+    const msgError = document.getElementById('index-action-error');
+    const toast = new bootstrap.Toast(msgError);
+    toast.show();
+};
+
+function validateEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateName(name) {
+    var regex = /^[a-zñáéíóúü ]{2,50}$/i;
+    return regex.test(name);
+}
+
+function validatePasswords(pass1, pass2) {
+    const re = /(?=.{8,})/;
+    return re.test(pass1) && re.test(pass2) && (pass1 === pass2);
+}
+
+function validatePassword(pass) {
+    return pass.trim() !== '';
+}
+
+const validateDataRegister = () => {
+    let existError = false;
+
+    if (!validateEmail(emailInput.value)) {
+        emailInput.classList.add('is-invalid');
+        errEmail.innerText = 'El correo electrónico es inválido';
+        existError = true;
+    } else {
+        emailInput.classList.remove('is-invalid');
+        errEmail.innerText = '';
+    }
+    if (!validateName(fullNameInput.value)) {
+        fullNameInput.classList.add('is-invalid');
+        errFName.innerText = 'El nombre es inválido';
+        existError = true;
+    } else {
+        fullNameInput.classList.remove('is-invalid');
+        errFName.innerText = '';
+    }
+    if (!validatePasswords(passwordInput.value, passwordConfirmInput.value)) {
+        passwordInput.classList.add('is-invalid');
+        passwordConfirmInput.classList.add('is-invalid');
+        errPass.innerText = 'Las contraseñas no coinciden o son menores a 8 caracteres';
+        existError = true;
+    } else {
+        passwordInput.classList.remove('is-invalid');
+        passwordConfirmInput.classList.remove('is-invalid');
+        errPass.innerText = '';
+    }
+
+    return existError;
+};
+
+buttonRegisterAction.addEventListener('click', function(e) {
+    e.preventDefault();
+    const validation = validateDataRegister();
+    if (!validation) {
+        const newUser = {
+            "full_name": fullNameInput.value,
+            "email": emailInput.value,
+            "password": passwordConfirmInput.value
+        };
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+        .then((res) => res.json())
+        .then((data) =>  {
+            localStorage.setItem(
+                'userSession',
+                JSON.stringify(newUser)
+            );
+            window.location.href = '/home.html';
+        })
+        .catch((err) => {
+            renderErrorMsg();
+        })
+    }
+});
+
+const validateDataLogin = () => {
+    let existError = false;
+    if (!validateEmail(emailLogin.value)) {
+        existError = true;
+        emailLogin.classList.add('is-invalid');
+        textErrorEmailLogin.innerText = 'El correo electróncio es inválido';
+    } else {
+
+    }
+    if (!validatePassword(passwordLogin.value)) {
+        existError = true;
+        passwordLogin.classList.add('is-invalid');
+        textErrorPassLogin.innerText = 'La contraseña es un campo requerido';
+    } else {
+
+    }
+
+    return existError;
+};
+
+const checkCorrectPassword = (passwordUser, passwordSaved) => {
+    return passwordUser === passwordSaved;
+};
+
+actionBtnLogin.addEventListener('click', function(e) {
+    e.preventDefault();
+    const existsErrors = validateDataLogin();
+    if (!existsErrors) {
+        fetch(`http://localhost:3000/users?email=${emailLogin.value}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((data) =>  {
+            if (data.length > 0) {
+                if (checkCorrectPassword(passwordLogin.value, data[0]['password'])) {
+                    localStorage.setItem(
+                        'userSession',
+                        JSON.stringify(data[0])
+                    );
+                    window.location.href = '/home.html';
+                } else {
+                    emailLogin.classList.add('is-invalid');
+                    textErrorEmailLogin.innerText = 'Las credenciales son inválidas';
+                    passwordLogin.classList.add('is-invalid');
+                    textErrorPassLogin.innerText = 'Las credenciales son inválidas';
+                }
+            }
+            if (data.length === 0) {
+                emailLogin.classList.add('is-invalid');
+                textErrorEmailLogin.innerText = 'El correo electrónico ingresado aún no ha sido registrado';
+            }
+        })
+        .catch((err) => {
+            renderErrorMsg();
+        })
+    }
+});
